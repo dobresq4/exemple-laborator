d83adb5e2d3aa276e46ebff65a50899fde24d977
Dobresq <andrei.dobrescu99@yahoo.com>
2021-11-04 18:03:37 +0200

    Lucrarea nr 2

---
+++ Lucrarea-02/Lucrarea2_DobrescuAndrei/Lucrarea2_DobrescuAndrei/Domain/EmptyShoppingCart.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucrarea2_DobrescuAndrei.Domain
+{
+    public record EmptyShoppingCart(string productCode, string quantity, string address, string price);
+}
---
+++ Lucrarea-02/Lucrarea2_DobrescuAndrei/Lucrarea2_DobrescuAndrei/PayShoppingCartWorkflow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Lucrarea2_DobrescuAndrei.Domain.ShoppingCartsPaidEvent;
+using static Lucrarea2_DobrescuAndrei.Domain.ShoppingCarts;
+using static Lucrarea2_DobrescuAndrei.Domain.ShoppingCartsOperations;
+
+namespace Lucrarea2_DobrescuAndrei.Domain
+{
+    public class PayShoppingCartWorkflow
+    {
+        public IShoppingCartsPaidEvent Execute(PayShoppingCartCommand command, Func<ProductCode, bool> checkProductExists)
+        {
+            EmptyShoppingCarts emptyShoppingCarts = new EmptyShoppingCarts(command.InputShoppingCarts);
+            IShoppingCarts shoppingCarts = ValidateShoppingCarts(checkProductExists, emptyShoppingCarts);
+            shoppingCarts = CalculateFinalPrice(shoppingCarts);
+            shoppingCarts = PayShoppingCart(shoppingCarts);
+
+            return shoppingCarts.Match(
+                    whenEmptyShoppingCarts: emptyShoppingCarts => new ShoppingCartsPaidFailedEvent("Unexpected unvalidated state") as IShoppingCartsPaidEvent,
+                    whenUnvalidatedShoppingCarts: unvalidatedShoppingCarts => new ShoppingCartsPaidFailedEvent(unvalidatedShoppingCarts.Reason),
+                    whenValidatedShoppingCarts: validatedShoppingCarts => new ShoppingCartsPaidFailedEvent("Unexpected validated state"),
+                    whenCalculatedShoppingCarts: calculatedShoppingCarts => new ShoppingCartsPaidFailedEvent("Unexpected calculated state"),
+                    whenPaidShoppingCarts: paidShoppingCarts => new ShoppingCartsPaidScucceededEvent(paidShoppingCarts.Csv, paidShoppingCarts.PublishedDate)
+                );
+        }
+    }
+}
